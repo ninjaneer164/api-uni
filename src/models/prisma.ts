7c3bcdbe55b9655c1f4db5e159073b0afdc4f33a
Hiroshi Miyazawa <ninjaneer164@gmail.com>
2023-11-17 07:36:44 -0600

    use ??

--- src/models/prisma.ts
+++ src/models/prisma.ts
@@ -4,6 +4,6 @@ export class PrismaClient {
   private static _prismaClient: PC;
 
   public static get instance(): PC {
-    return PrismaClient._prismaClient || new PC();
+    return PrismaClient._prismaClient ?? new PC();
   }
 }
